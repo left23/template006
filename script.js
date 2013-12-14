456d4fe372885595457e98d50edf9fa1a034a65c
left23 <lcdemc@gmail.com>
2013-12-14 23:03:05 +0000

    added an armoury to the scene

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+int fontsize = 24;
+
 var sword=false;
 var gold = 10;
 var bread = 2;
@@ -17,7 +20,8 @@ var legSpeed=[];
 var legsmoving=[];
 var legsmovingspeed=[];
 var enhp=[];
-for(var i=0;i<10;i++){
+
+for(var i=0;i<5;i++){
     enX[i]=-i;
     wait[i]=false;
     legsmoving[i]=false;
@@ -28,6 +32,8 @@ for(var i=0;i<10;i++){
     legsmovingspeed[i]=random(3,5);
     enhp[i]=100;
 }
+
+
 var playerX=200;
 var pswordRotate=20;
 var protateSpeed=0;
@@ -37,18 +43,8 @@ var plegSpeed=0;
 var plegsmoving=false;
 var pwait=false;
 var townsa="No other towns unlocked";
-frameRate(30);
-
-
-void setup() {
-		    size(568, 380);
-		 //    background(#F0F0E0);
-		 //   stroke(0);
-		    fill(0);
-		    textFont(createFont("Arial",fontsize));
-	 //	    redraw();
-		   
-      };
+//frameRate(30);
+var hp=100;
 
 
 void keyReleased(){
@@ -63,9 +59,341 @@ void keyReleased(){
 
 
 
-var hp=100;
+		void setup() {
+		    size(568, 380);
+		 //    background(#F0F0E0);
+		 //   stroke(0);
+		    fill(0);
+		    textFont(createFont("Arial",fontsize));
+	//	    redraw();
+		   
+      }
 
-var myfont=createFont("serif",17);
+var myfont=createFont("sans-serif",17);
+
+
+
+void draw () {
+
+ noStroke();
+    textFont(myfont,17);
+    if(area==="town"){
+        background(0, 196, 255);//sky
+        fill(0, 255, 68);//grass
+        rect(0,300,568,380);//grass
+		
+        fill(97, 66, 66);//tree trunk
+        rect(45,200,20,100);//tree trunk
+        fill(43, 179, 22);//tree leaves
+        ellipse(55,200,70,70);//tree leaves
+		
+        fill(255, 255, 0);//sun
+        ellipse(0,0,90,90);//sun
+		
+		
+        fill(135, 206, 250);//cloud
+		ellipse(0,110,100,60);//cloud
+        fill(132, 200, 240);//cloud
+        ellipse(220,70,140,70);//cloud
+        ellipse(290,80,140,70);//cloud
+		
+        fill(255, 255, 255);//cloud1
+        ellipse(90,80,140,70);//cloud1
+        fill(244, 244, 255);//cloud1
+        ellipse(160,110,100,60);//cloud1
+        ellipse(120,70,140,70);//cloud1
+		
+        fill(244, 244, 255);//cloud2
+		ellipse(460,130,150,50);//cloud1
+        ellipse(400,110,130,60);//cloud2
+        fill(255, 255, 255);//cloud2
+        ellipse(420,80,145,75);//cloud2
+		
+        fill(78, 45, 45);//tree trunk1
+        rect(350,200,10,100);//tree trunk1
+        fill(43, 233, 22);//tree leaves1
+        ellipse(355,200,70,70);//tree leaves1
+		
+        fill(78, 45, 45);//tree trunk2
+        rect(380,200,10,100);//tree trunk2
+        fill(22, 196, 22);//tree leaves2
+        ellipse(385,200,95,80);//tree leaves2
+		
+        fill(173, 173, 173);//bakery
+        rect(120,230,80,50);//bakery facade
+        rect(110,250,100,50);//bakery main building
+        fill(89, 68, 68);//bakery door
+        arc(160,300,40,70,-109,0);//bakery door
+	
+        fill(0, 255, 68);//bakery door cut off
+        rect(130,300,60,40);//bakery door cut off
+
+        fill(10, 10, 10);//bakery sign
+        text("Bakery",135,250);//bakery text
+		
+		
+		
+        fill(230, 20, 120);//barracks
+        rect(435,180,80,120);//barracks tower
+		
+        fill(0, 196, 255);//barracks tower cut out
+        rect(445,180,12,10);//barracks tower cut out
+        rect(467,180,12,10);//barracks tower cut out
+        rect(489,180,12,10);//barracks tower cut out
+		
+        fill(200, 10, 100);//barracks
+        rect(410,230,130,70);//barracks main building
+		
+        fill(0, 196, 255);//barracks cut out
+        rect(420,230,10,10);//barracks cut out
+        fill(230, 20, 120);//barracks cut out
+        rect(440,230,10,10);//barracks cut out
+        rect(460,230,10,10);//barracks cut out
+        rect(480,230,10,10);//barracks cut out
+        rect(500,230,10,10);//barracks cut out
+        fill(0, 196, 255);//barracks cut out
+        rect(520,230,10,10);//barracks cut out
+		
+        fill(0, 0, 0);//barracks door
+        rect(445,270,60,30);//barracks door
+	    fill(255, 255, 255);//barracks sign
+        text("Barracks",440,260);//barracks text
+		
+		
+		
+		
+		
+        drawButton("Barracks",313,344,100);
+        if(over_button&&pressed){
+            area="barracks";
+        }
+		
+        drawButton("Bakery",107,311,100);
+        if(over_button&&pressed){
+            area="bakery";
+        }
+        drawButton("Forest",4,311,100);
+        if(over_button&&pressed){
+            area="forest";
+        }
+        textFont(myfont,12);
+        fill(102, 74, 74);
+        rect(230,250,60,50,10);
+        rect(230,260,60,40);
+        fill(145, 145, 145);
+        text("Market",240,265);
+        textFont(myfont,17);
+        rect(245,270,30,30);
+        drawButton("Market",210,311,100);
+        if(over_button&&pressed){
+            area="market";
+        }
+        drawButton("Credits",107,344,100);
+        if(over_button&&pressed){
+            area="credits";
+        }
+        drawButton("Inventory",4,344,100);
+        if(over_button&&pressed){
+            area="inventory";
+        }
+        drawButton("Battle",313,311,84);
+        if(over_button&&pressed){
+            area="battle";
+            for(var i=0;i<5;i++){
+                enhp[i]=100;
+                enX[i]=-50*i;
+            }
+        }
+        drawButton("More Towns",210,344,100);
+        if(over_button&&pressed){
+            area="towns";
+        }
+    }
+    if(area==="battle"){
+        stroke(0, 0, 0);
+        drawBattle();
+        if(enhp[4]<10){
+            drawButton("Back",150,50,100);
+            if(over_button&&pressed){
+                area="town";townsa="Ne"+"w Town";
+            }
+        }
+    }
+    if(area==="forest"){
+        background(0, 196, 255);
+        fill(0, 255, 68);
+        rect(0,300,568,380);
+        fill(97, 66, 66);
+        if(wood===false){
+            rect(120,200,20,100);
+            rect(300,200,20,100);
+            rect(220,200,20,100);
+            rect(400,200,20,100);
+            fill(43, 179, 22);
+            ellipse(310,200,75,75);
+            ellipse(230,200,75,75);
+            ellipse(410,200,75,75);
+            ellipse(130,200,75,75);
+        }
+        else{
+            rect(120,280,20,20);
+            rect(300,280,20,20);
+            rect(220,280,20,20);
+            rect(400,280,20,20);
+        }
+        if(mousePressed&&mouseY>180){
+            wood=true;
+        }
+        drawButton("Back",10,10,75);
+        if(over_button&&pressed){
+            area="town";
+        }
+    }
+    if(area==="market"){
+        background(0, 196, 255);
+        fill(0, 255, 68);
+        rect(0,300,568,380);
+        fill(0);
+        text("Gold: "+gold,122,72);
+        drawButton("Back",10,10,75);
+        if(over_button&&pressed){
+            area="town";
+        }
+        drawButton("Sell wood",10,50,100);
+        if(over_button&&pressed&&wood){
+            gold+=10;
+            wood=false;
+        }
+        if(sword===false){
+            drawButton("Buy a better sword for 100 gold",10,90,259);
+            if(over_button&&pressed&&gold>99){
+                gold-=100;
+                sword=true;
+            }
+        }
+    }
+	
+	
+	
+	
+	
+    if(area==="barracks"){
+        background(0, 196, 255);
+        fill(0, 255, 68);
+        rect(0,300,568,380);
+        fill(0);
+        text("Gold: "+gold,122,72);
+        drawButton("Back",10,10,75);
+        if(over_button&&pressed){
+            area="town";
+        }
+        drawButton("Sell weapons",10,50,100);
+        if(over_button&&pressed&&wood){
+            gold+=10;
+            wood=false;
+        }
+        if(sword===false){
+            drawButton("Buy a better sword for 100 gold",10,90,259);
+            if(over_button&&pressed&&gold>99){
+                gold-=100;
+                sword=true;
+            }
+        }
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    if(area==="bakery"){
+        background(0, 196, 255);
+        fill(0, 255, 68);
+        rect(0,300,568,380);
+        drawButton("Back",10,10,75);
+        if(over_button&&pressed){
+            area="town";
+        }
+        drawButton("Buy Bread for 5 gold",100,200,200);
+        text("Current amount of bread: "+bread,100,251);
+        if(over_button&&pressed&&gold>4){
+            bread++;
+            gold-=5;
+        }
+    }
+    if(area==="credits"){
+        background(0, 196, 255);
+        fill(0, 255, 68);
+        rect(0,300,568,380);
+        drawButton("Back",10,10,75);
+        if(over_button&&pressed){
+            area="town";
+        }
+        text("Ari(Aragorn),\ntimothy.smith\nThis game is dedicated to my friends\nHarrison and MilkyWay.",50,200);
+    }
+    if(area==="towns"){
+        background(0, 196, 255);
+        fill(0, 255, 68);
+        rect(0,300,568,380);
+        drawButton("Back",10,10,75);
+        if(over_button&&pressed){
+            area="town";
+        }
+        drawButton("Visit the forest of Carn",10,45,189);
+        if(over_button&&pressed){
+            area="carn";
+        }
+    }
+    if(area==="inventory"){
+        background(102, 74, 74);
+        drawButton("Back",10,10,75);
+        if(over_button&&pressed){
+            area="town";
+        }
+        text("Supplies:\n"+gold+" gold\n"+bread+" bread\nHP: "+hp+"%",200,200);
+        drawButton("Eat Bread",200,285,85);
+        if(over_button&&pressed&&bread>0){
+            hp+=5;
+            bread--;
+        }
+        translate(0,20);
+    fill(255, 173, 51);
+    rect(167,206,31,17,5);
+    fill(219, 144, 59);
+    arc(182,206,25,7,6,176);
+    fill(150);
+    ellipse(189,195,15,15);
+    resetMatrix();
+    if(sword===true){
+        fill(0);
+        text("\n\n\n\nSword",200,200);
+    }
+    }
+    if(area==="carn"){
+        background(0, 196, 255);
+        fill(0, 255, 68);
+        rect(0,300,568,380);
+        drawButton("Back",5,5,59,100);
+        if(over_button&&pressed){
+            area="town";
+        }
+    }
+    pressed=false;
+   
+};
+
+
+
+
+
 
 void drawButton (button_text, x, y, length) {
     textFont(myfont,17);
@@ -92,20 +420,21 @@ void drawButton (button_text, x, y, length) {
     textAlign(LEFT, 0);
 };
 
+
+
 void mouseReleased(){
     pressed=true;
 };
 
 
-
-//////////////////////////////////////////////////////////////////////////////////////
-void draw() {
-    noStroke();
+void drawBattle() {
+    background(0, 196, 255);
+    noStroke(); noStroke();
     textFont(myfont,17);
     if(area==="town"){
         background(0, 196, 255);
         fill(0, 255, 68);
-        rect(0,300,400,150);
+        rect(0,300,568,380);
         fill(97, 66, 66);
         rect(45,200,20,100);
         fill(43, 179, 22);
@@ -195,7 +524,7 @@ void draw() {
     if(area==="market"){
         background(0, 196, 255);
         fill(0, 255, 68);
-        rect(0,300,400,150);
+        rect(0,300,568,380);
         fill(0);
         text("Gold: "+gold,122,72);
         drawButton("Back",10,10,75);
@@ -218,7 +547,7 @@ void draw() {
     if(area==="bakery"){
         background(0, 196, 255);
         fill(0, 255, 68);
-        rect(0,300,400,150);
+        rect(0,300,568,380);
         drawButton("Back",10,10,75);
         if(over_button&&pressed){
             area="town";
@@ -233,7 +562,7 @@ void draw() {
     if(area==="credits"){
         background(0, 196, 255);
         fill(0, 255, 68);
-        rect(0,300,400,150);
+        rect(0,300,568,380);
         drawButton("Back",10,10,75);
         if(over_button&&pressed){
             area="town";
@@ -243,7 +572,7 @@ void draw() {
     if(area==="towns"){
         background(0, 196, 255);
         fill(0, 255, 68);
-        rect(0,300,400,150);
+        rect(0,300,568,380);
         drawButton("Back",10,10,75);
         if(over_button&&pressed){
             area="town";
@@ -281,25 +610,13 @@ void draw() {
     if(area==="carn"){
         background(0, 196, 255);
         fill(0, 255, 68);
-        rect(0,300,400,150);
+        rect(0,300,568,380);
         drawButton("Back",5,5,59,100);
         if(over_button&&pressed){
             area="town";
         }
     }
     pressed=false;
-};
-
-
-/////////////////////////////////////////////////////// End void draw ////////////////////////////////
-
-
-
-
-
-void drawBattle() {
-    background(0, 196, 255);
-    noStroke();
     fill(125, 82, 82);
     ellipse(200,370,250,150);
     fill(97, 72, 72);
@@ -366,7 +683,7 @@ void drawBattle() {
         rect(-29,7,33,5,2);
     }
     resetMatrix();
-    if(keyPressed&&keyCode===DOWN&&pswordRotate===20){
+    if(keyIsPressed&&keyCode===DOWN&&pswordRotate===20){
         protateSpeed=10;
     }
     pswordRotate+=protateSpeed;
@@ -377,12 +694,12 @@ void drawBattle() {
         pswordRotate=20;
         protateSpeed=0;
     }
-    if(keyPressed&&keyCode===RIGHT){
+    if(keyIsPressed&&keyCode===RIGHT){
         playerX+=5;
         pfacingLeft=false;
         plegsmoving=true;
     }
-    if(keyPressed&&keyCode===LEFT){
+    if(keyIsPressed&&keyCode===LEFT){
         playerX-=5;
         pfacingLeft=true;
         plegsmoving=true;
@@ -513,4 +830,6 @@ void drawBattle() {
     }
 };
 
-////////////////////////////////////////////////////////////////
+
+
+
